99b340f2d32a6d5c93e2db535b9876ca3b5229ba
Mateusz Bober <mateusz.bober@wpengine.com>
2023-06-16 10:56:17 +0100

    Moved folder out of nested folder structure

---
+++ todo-app/.next/server/flight-server-css-manifest.js
@@ -0,0 +1,14 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/mateusz.bober/javascriptproject/todo-app/todo-app/app/layout.js": [
+      "/Users/mateusz.bober/javascriptproject/todo-app/todo-app/app/globals.css",
+      "/Users/mateusz.bober/javascriptproject/todo-app/todo-app/node_modules/next/font/google/target.css?{\"path\":\"app/layout.js\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  },
+  "cssModules": {
+    "/Users/mateusz.bober/javascriptproject/todo-app/todo-app/app/page": [
+      "/Users/mateusz.bober/javascriptproject/todo-app/todo-app/app/globals.css",
+      "/Users/mateusz.bober/javascriptproject/todo-app/todo-app/node_modules/next/font/google/target.css?{\"path\":\"app/layout.js\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  }
+}
